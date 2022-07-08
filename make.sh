15e1ec76f3745b1b9526b67236bcc65c5bb3e7d7
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-07-07 19:54:19 -0700

    Random make changes

--- make.sh
+++ make.sh
@@ -4,12 +4,15 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-clang++ \
-  -Wall \
-  -D GC_ALLOC=1 \
-  -fsanitize=address \
+echo "LOC : $(find . -name \*.cc -o -name \*.h|xargs cat | wc -l )"
+
+clang++                   \
+  -Wall                   \
+  -D GC_ALLOC=1           \
+  -fsanitize=address      \
   -fno-omit-frame-pointer \
-  -ggdb \
+  -ggdb                   \
   test.cc -o __test_gc
+
 ./__test_gc
 
